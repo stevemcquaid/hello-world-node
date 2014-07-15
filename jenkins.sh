bd36f94f89ee181a7c7aceb7217af5c9a9d4b75f
Stephen McQuaid <smcquaid@godaddy.com>
2014-07-15 13:29:34 -0700

    updated jenkins script

--- jenkins.sh
+++ jenkins.sh
@@ -1,15 +1,5 @@
-echo "Host github.secureserver.net
-    StrictHostKeyChecking no" >> ~/.ssh/config
+pwd
 
-git clone https://github.secureserver.net/smcquaid/hello-world-node.git
 ls
-
-git clone git@github.secureserver.net:smcquaid/hello-world-node.git
-ls
-
-
-cd ~/.ssh
-ls
-cat ~/.ssh/id_rsa.pub
 
 node hello.js
