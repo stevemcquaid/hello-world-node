62b8e1859e4768df3a0905f07d2d4a45cff4e2c5
Stephen McQuaid <smcquaid@godaddy.com>
2014-07-15 15:49:16 -0700

    cleaned up jenkins output with echos

--- jenkins.sh
+++ jenkins.sh
@@ -1,10 +1,14 @@
+echo "---- Present Working Directory:"
 pwd
 
+echo "---- List of all files within current directory:"
 ls
 
+echo "---- Building Docker container... "
 #build the image
 docker build -t hello-world-node .
 
+echo "---- Running the Docker container... "
 #kickoff/start the node app.
 #nodejs == node becuase ubuntu is wicked weird.
 docker run hello-world-node nodejs /src/hello.js
