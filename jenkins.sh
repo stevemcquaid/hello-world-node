6db80f6d054cbbf836387e5e522cc52f6ba77583
Stephen McQuaid <smcquaid@godaddy.com>
2014-07-16 17:53:26 -0700

    untested but YOLO

--- jenkins.sh
+++ jenkins.sh
@@ -1,28 +1,33 @@
+##System Vars Here
+HELLOWORLDNODEHOST=n-smcquaid2.dev-godaddy.com
+
+echo "---- Have you setup your system vars?"
+echo $HELLOWORLDNODEHOST
+
 echo "---- Present Working Directory:"
 pwd
 
 echo "---- List of all files within current directory:"
 ls
 
+
+
+### Pull and run nginx-proxy container
+### This will put hello-world-node under a reverse proxy and serve it properly when navigating to the domain listed above.
+git clone https://github.com/jwilder/nginx-proxy.git
+cd nginx-proxy
+docker run -d -p 80:80 -v /var/run/docker.sock:/tmp/docker.sock -t jwilder/nginx-proxy
+cd ..
+
+
+
 echo "---- Building Docker container... "
 #build the image
-docker build -t hello-world-node .
+docker build -t smcquaid/hello-world-node .
 
 echo "---- Running the Docker container... "
 #kickoff/start the node app.
 #nodejs == node becuase ubuntu is wicked weird.
-#docker run -d -P hello-world-node nodejs /src/hello.js ### Can't use this due to security group port restrictions
-CID=$(docker run -d -p 60015:9009 hello-world-node nodejs /src/hello.js);
-
-dockerip='docker ps | tail -n +2 | while read cid b; do echo -n "$cid\t"; docker inspect $cid | grep IPAddress | cut -d \" -f 4; done'
-
-docker inspect --format '{{ .NetworkSettings.IPAddress }}' ${CID}
-
-echo $dockerip
-
-echo "This should auto-bind to available port"
-
-VAR=docker ps
-echo $VAR
+docker run -d -P -e VIRTUAL_HOST=$HELLOWORLDNODEHOST smcquaid/hello-world-node nodejs /src/hello.js
 
 echo "---- DONE! ----"
